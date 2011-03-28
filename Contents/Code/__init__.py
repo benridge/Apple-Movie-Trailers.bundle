c81a8def43d340551509a89d46100a9bdd9a661c
sander1 <sanderspies+plexapp@gmail.com>
2011-03-28 20:13:37 +0200

    Use iTunes xml files to lookup the different trailer formats and urls

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,178 +1,189 @@
-
 import re
 
-AMT_PLUGIN_PREFIX   = "/video/amt"
-AMT_SITE_URL        = "http://trailers.apple.com"
-AMT_JSON_URL        = "http://trailers.apple.com/trailers/home/feeds/%s.json"
-AMT_THUMB_URL       = "http://images.apple.com/trailers"
-AMT_SEARCH_URL      = "http://trailers.apple.com/trailers/home/scripts/quickfind.php?callback=searchCallback&q=%s"
+AMT_SITE_URL   = 'http://trailers.apple.com'
+AMT_JSON_URL   = 'http://trailers.apple.com/trailers/home/feeds/%s.json'
+AMT_SEARCH_URL = 'http://trailers.apple.com/trailers/home/scripts/quickfind.php?q=%s'
+AMT_VIDEOS     = 'http://trailers.apple.com/moviesxml%sindex.xml'
+AMT_VIDEOS_NS  = {'a':'http://www.apple.com/itms/'}
+
+ART          = 'art-default.jpg'
+ICON_DEFAULT = 'logo.png'
+ICON_SEARCH  = 'icon-search.png'
+ICON_PREFS   = 'icon-prefs.png'
+
+PREF_MAP = {'1080p':'1080p', '720p':'720p', '480p':'480p', 'Large':'640w', 'Medium':'480', 'Small':'320'}
+ORDER    = ['1080p', '720p', '480p', '640w', '480', '320']
 
 ####################################################################################################
+
 def Start():
-  # Curent artwork.jpg free for personal use only - http://squaresailor.deviantart.com/art/Apple-Desktop-52188810
-  Plugin.AddPrefixHandler(AMT_PLUGIN_PREFIX, MainMenu, L('Apple Movie Trailers'), 'icon-default.png', 'art-default.jpg')
+  # Current artwork.jpg free for personal use only - http://squaresailor.deviantart.com/art/Apple-Desktop-52188810
+  Plugin.AddPrefixHandler('/video/amt', MainMenu, 'Apple Movie Trailers', ICON_DEFAULT, ART)
   Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
-  MediaContainer.title1 = 'Apple Movie Trailers'
-  MediaContainer.userAgent = 'Apple Mac OS X v10.6.1 CoreMedia v1.0.0.10B504'
-  MediaContainer.viewGroup = 'List'
-  MediaContainer.art = R('art-default.jpg')
-  DirectoryItem.thumb = R('icon-default.png')
-  HTTP.CacheTime = 7200
-  HTTP.Headers['User-agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11'
+  Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
 
+  MediaContainer.title1 = 'Apple Movie Trailers'
+  MediaContainer.userAgent = 'Apple Mac OS X v10.6.7 CoreMedia v1.0.0.10J869'
+  MediaContainer.art = R(ART)
+  MediaContainer.viewGroup = 'List'
+
+  DirectoryItem.thumb = R(ICON_DEFAULT)
+  VideoItem.thumb = R(ICON_DEFAULT)
+
+  HTTP.CacheTime = 7200
+  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-us) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27'
 
 ####################################################################################################
-def MainMenu():
-    dir = MediaContainer(title1="Apple Movie Trailers")
-    dir.Append(Function(DirectoryItem(JSONMenu, title="Just Added"), name="just_added"))
-    dir.Append(Function(DirectoryItem(JSONMenu, title="Exclusive"), name="exclusive"))
-    dir.Append(Function(DirectoryItem(JSONMenu, title="Just HD", thumb=R("thumb-just_hd.png")), name="just_hd"))
-    dir.Append(Function(DirectoryItem(JSONMenu, title="Most Popular"), name="most_pop"))
-    dir.Append(Function(DirectoryItem(GenresMenu, title="Genres")))
-    dir.Append(Function(DirectoryItem(StudiosMenu, title="Movie Studios")))
-    dir.Append(Function(InputDirectoryItem(Search, title="Search trailers", prompt="Search for movie trailer", thumb=R("icon-search.png"))))
-    dir.Append(PrefsItem(title="Preferences", thumb=R("icon-prefs.png")))
-    return dir
 
-def JSONMenu(sender, name, query=None):
-	return ContainerFromJSONName(name)
-	
-def GenresMenu(sender, query=None):
-  dir = MediaContainer(title1="Apple Movie Trailers", title2="Genres")
-  dict = JSON.ObjectFromURL(AMT_JSON_URL % "genres")
+def MainMenu():
+  dir = MediaContainer()
+
+  dir.Append(Function(DirectoryItem(JsonMenu, title='Just Added'), name='just_added'))
+  dir.Append(Function(DirectoryItem(JsonMenu, title='Exclusive'), name='exclusive'))
+  dir.Append(Function(DirectoryItem(JsonMenu, title='Just HD', thumb=R('thumb-just_hd.png')), name='just_hd'))
+  dir.Append(Function(DirectoryItem(JsonMenu, title='Most Popular'), name='most_pop'))
+  dir.Append(Function(DirectoryItem(GenresMenu, title='Genres')))
+  dir.Append(Function(DirectoryItem(StudiosMenu, title='Movie Studios')))
+  dir.Append(Function(InputDirectoryItem(Search, title='Search Trailers', prompt='Search for movie trailer', thumb=R(ICON_SEARCH))))
+  dir.Append(PrefsItem(title='Preferences', thumb=R(ICON_PREFS)))
+
+  return dir
+
+####################################################################################################
+
+def JsonMenu(sender, name):
+  dir = MediaContainer(title2=sender.itemTitle)
+
+  for trailer in JSON.ObjectFromURL(AMT_JSON_URL % name):
+    url   = trailer['location']
+    title = trailer['title']
+    thumb = trailer['poster']
+    dir.Append(Function(DirectoryItem(Videos, title=title, thumb=Function(Thumb, url=thumb)), url=url, thumb=thumb))
+
+  return dir
+
+####################################################################################################
+
+def GenresMenu(sender):
+  dir = MediaContainer(title2='Genres')
   genres = []
-  for item in dict:
-    for genre in item["genre"]:
+  for trailer in JSON.ObjectFromURL(AMT_JSON_URL % 'genres'):
+    for genre in trailer['genre']:
       if genre not in genres:
         genres.append(genre)
   genres.sort()
   for genre in genres:
     dir.Append(Function(DirectoryItem(GenreMenu, title=genre)))
+
   return dir
 
-def GenreMenu(sender, query=None):
-  dir = MediaContainer(title1="Genres", title2=sender.itemTitle)
-  dict = JSON.ObjectFromURL(AMT_JSON_URL % "genres")
-  for item in dict:
-    if sender.itemTitle in item["genre"]:
-      key = AMT_SITE_URL + item["location"]
-      thumb = item["poster"]
-      dir.Append(Function(VideoItem(getVideo, title=item["title"], thumb=thumb), url=key))
+####################################################################################################
+
+def GenreMenu(sender):
+  dir = MediaContainer(title2=sender.itemTitle)
+
+  for trailer in JSON.ObjectFromURL(AMT_JSON_URL % 'genres'):
+    if sender.itemTitle in trailer['genre']:
+      url   = trailer['location']
+      title = trailer['title']
+      thumb = trailer['poster']
+      dir.Append(Function(DirectoryItem(Videos, title=title, thumb=Function(Thumb, url=thumb)), url=url, thumb=thumb))
+
   return dir
 
-def StudiosMenu(sender, query=None):
-    dir = MediaContainer(title1="Apple Movie Trailers", title2="Studios")
-    dict = JSON.ObjectFromURL(AMT_JSON_URL % "studios")
-    studios = []
-    for item in dict:
-      if item["studio"] not in studios:
-        dir.Append(Function(DirectoryItem(StudioMenu, title=item["studio"])))
-        studios.append(item["studio"])
+####################################################################################################
+
+def StudiosMenu(sender):
+  dir = MediaContainer(title2='Studios')
+  studios = []
+  for trailer in JSON.ObjectFromURL(AMT_JSON_URL % 'studios'):
+    if trailer['studio'] not in studios:
+      studios.append(trailer['studio'])
+  studios.sort()
+  for studio in studios:
+    dir.Append(Function(DirectoryItem(StudioMenu, title=studio)))
+
+  return dir
+
+####################################################################################################
+
+def StudioMenu(sender):
+  dir = MediaContainer(title2=sender.itemTitle)
+  for trailer in JSON.ObjectFromURL(AMT_JSON_URL % 'studios'):
+    if trailer['studio'] == sender.itemTitle:
+      url   = trailer['location']
+      title = trailer['title']
+      thumb = trailer['poster']
+      dir.Append(Function(DirectoryItem(Videos, title=title, thumb=Function(Thumb, url=thumb)), url=url, thumb=thumb))
+
+  return dir
+
+####################################################################################################
+
+def Search(sender, query):
+  dir = MediaContainer(title2=sender.itemTitle)
+
+  for trailer in JSON.ObjectFromURL(AMT_SEARCH_URL % String.Quote(query))['results']:
+    url   = trailer['location']
+    title = trailer['title'].replace('%u2019', "'")
+    thumb = AMT_SITE_URL + trailer['poster']
+    dir.Append(Function(DirectoryItem(Videos, title=title, thumb=Function(Thumb, url=thumb)), url=url, thumb=thumb))
+
+  if len(dir) == 0:
+    return MessageContainer('No results', 'Your search query didn\'t return any matches')
+  else:
     return dir
 
-def StudioMenu(sender, query=None):
-  dir = MediaContainer(title1="Studios", title2=sender.itemTitle)
-  dict = JSON.ObjectFromURL(AMT_JSON_URL % "studios")
-  for item in dict:
-    if item["studio"] == sender.itemTitle:
-      key = AMT_SITE_URL + item["location"]
-      thumb = item["poster"]
-      dir.Append(Function(VideoItem(getVideo, title=item["title"], thumb=thumb), url =key))
+####################################################################################################
+
+def Videos(sender, url, thumb):
+  dir = MediaContainer(viewGroup='InfoList', title2=sender.itemTitle)
+
+  xml = XML.ElementFromURL(AMT_VIDEOS % url.replace('trailers', 's'), errors='ignore')
+  summary = xml.xpath('//a:ScrollView//comment()[contains(.,"DESCRIPTION")]/following-sibling::a:TextView[1]/a:SetFontStyle', namespaces=AMT_VIDEOS_NS)[0].text.strip()
+
+  for video in xml.xpath('//a:HBoxView/a:GotoURL', namespaces=AMT_VIDEOS_NS):
+    title = video.xpath('.//parent::a:HBoxView//a:b', namespaces=AMT_VIDEOS_NS)[0].text
+    url = AMT_SITE_URL + video.get('url')
+    dir.Append(Function(VideoItem(PlayVideo, title=title, summary=summary, thumb=Function(Thumb, url=thumb)), url=url))
+
   return dir
 
 ####################################################################################################
-def Search(sender, query):
-  callback = HTTP.Request(AMT_SEARCH_URL % String.Quote(query)).content
-  if callback is None: return None
-  callback = callback.lstrip("searchCallback(")[:-3]
-  d = JSON.ObjectFromString(callback)
-  dir = MediaContainer(title1="Search", title2=sender.itemTitle)
-  for item in d["results"]:
-    if item["location"].find("phobos.apple.com") == -1:
-      thumb = AMT_SITE_URL + item["poster"]
-      dir.Append(Function(VideoItem(getVideo, title=item["title"], thumb=thumb), url=AMT_SITE_URL + item["location"]))
-  if len(dir) == 0:
-    dir.Append(DirectoryItem("%s/search" % AMT_PLUGIN_PREFIX, "(No Results)", ""))
-  return dir
+
+def PlayVideo(sender, url):
+  user_quality = Prefs['VideoQuality']
+  pref_value = PREF_MAP[user_quality]
+  available = {}
+
+  for res in XML.ElementFromURL(url).xpath('//a:TrackList//a:array/a:dict', namespaces=AMT_VIDEOS_NS):
+    video_url = res.xpath('./a:key[text()="previewURL"]/following-sibling::*[1]', namespaces=AMT_VIDEOS_NS)[0].text
+    q = re.search('_h(320|480|640w|480p|720p|1080p)\.mov$', video_url)
+    if q:
+      q = q.group(1)
+      if q in ORDER:
+        available[q] = video_url
+
+  for i in range(ORDER.index(pref_value), len(ORDER)):
+    quality = ORDER[i]
+    if quality in available:
+      video_url = available[quality]
+      break
+
+  return Redirect(video_url)
 
 ####################################################################################################
-def ContainerFromJSONName(jsonName):
-  HTTP.Request('http://www.apple.com/trailers/universal/thevampiresassistant/')
-  cookies = HTTP.GetCookiesForURL(AMT_SITE_URL)
-  
-  dir = MediaContainer(title1="Apple Movie Trailers", title2=L(jsonName), httpCookies=cookies)
-  dict = JSON.ObjectFromURL(AMT_JSON_URL % jsonName)
-  
-  for item in dict:
-    key = AMT_SITE_URL + item["location"]
-    thumb = item["poster"]
-    dir.Append(Function(VideoItem(getVideo, title=item["title"], thumb=thumb), url=key))
-  return dir
 
-####################################################################################################
-def getTrailers(html):
-  # First look for trailers.
-  html = str(html)
-  trailers = re.findall('"(http://[^"]+tlr[^"]+[1-9][0-9]+[a-z]?\.mov[^"]*)"', html)
-  if len(trailers) == 0:
-    trailers = re.findall('"(http://[^"]+[1-9][0-9]+[a-z]?\.mov[^"]*)"', html)
-  if len(trailers) == 0:
-    matches = XML.ElementFromString(html, True).xpath('//li[@class="hd"]/a')
-    for match in matches:
-       content = match.get('href')
-       if content.find('http') > -1:
-          movUrl = content
-       else:
-          movUrl = AMT_SITE_URL + content
-       if movUrl.find('.mov') > -1:
-       		trailers.append(movUrl)
-  return trailers
+def Thumb(url):
+  try:
+    large_thumb = url.replace('.jpg', '-large.jpg')
+    data = HTTP.Request(large_thumb, cacheTime=CACHE_1MONTH).content
+    return DataObject(data, 'image/jpeg')
+  except:
+    try:
+      data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
+      return DataObject(data, 'image/jpeg')
+    except:
+      pass
 
-####################################################################################################
-def getVideo(sender, url):
-  url = url + "/includes/playlists/web.inc"
-  userQuality = Prefs["VideoQuality"]
-
-  order = ['1080p', '720p', '480p', '640w', '480', '320']
-  prefMap = {'1080p':'1080p', '720p':'720p', '480p':'480p', 'Large':'640w', 'Medium':'480', 'Small':'320' }
-  prefValue = prefMap[userQuality]
-  
-  # Get all the trailers.
-  html = HTTP.Request(url)
-  trailers = getTrailers(html)
-
-  if len(trailers) == 0:
-    # Could be a special landing page.
-    matches = XML.ElementFromString(html, True).xpath("//div[@class='trailerlinks']//area[@alt='HD']")
-    if len(matches) > 0:
-      match = matches[1].get('href')
-      html = HTTP.Request(url + match)
-      trailers = getTrailers(html)
-  
-  trailerList = []
-  for trailer in trailers:
-    t = trailer.replace('_h.', '_h').replace('h640','h640w')
-    if t.find('480p') != -1 and t.find('h480p') == -1:
-    	t = t.replace('480p','h480p')
-    if t.find('720p') != -1 and t.find('h720p') == -1:
-    	t = t.replace('720p','h720p')
-    if t.find('1080p') != -1 and t.find('h1080p') == -1:
-    	t = t.replace('1080p','h1080p')
-    
-    #t = t.replace('480p','h480p').replace('720p','h720p').replace('1080p','h1080p')
-    size = re.findall('([0-9]+[a-z]?)\.mov', t)[0]
-    trailerList.append([size, t])
-
-  # Find the appropriate one.
-  desired = order.index(prefValue)
-  for (size,url) in trailerList:
-    found = order.index(size)
-    Log("Found=%d, desired=%d" % (found, desired))
-    if found == desired:
-      Log("Using:"+url)
-      return Redirect(url)
-      
-  # If we didn't find a match, then return the highest resolution one we can find.
-  if len(trailerList) > 0:
-    return Redirect(trailerList[-1][1])
-      
+  return Redirect(R(ICON_DEFAULT))
