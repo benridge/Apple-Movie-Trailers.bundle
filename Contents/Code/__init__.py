090190e52c205e741cb9cfed492744431a4a83fa
James Clarke <james@plexapp.com>
2012-12-13 08:57:24 -0800

    Removed bundled search thumb, added search term.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -38,7 +38,7 @@ def MainMenu():
   oc.add(DirectoryObject(key=Callback(Categories, name='most_pop'), title=L('most_pop')))
   oc.add(DirectoryObject(key=Callback(Genres), title=L('genres')))
   oc.add(DirectoryObject(key=Callback(Studios), title=L('movie_studios')))
-  oc.add(SearchDirectoryObject(identifier='com.plexapp.plugins.amt', title='Search Trailers', prompt='Search for movie trailer', thumb=R('icon-search.png')))
+  oc.add(SearchDirectoryObject(identifier='com.plexapp.plugins.amt', title='Search Trailers', prompt='Search for movie trailer', term=L('Trailers')))
 
   return oc
 
