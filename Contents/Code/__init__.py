ed08b5683842443758dbfe01d7eff99e1f30ef80
sander1 <sander@plexapp.com>
2013-07-27 16:35:37 +0200

    Make it work with PlexConnect

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,4 @@
-AMT_SITE_URL = 'http://trailers.apple.com'
-AMT_JSON_URL = 'http://trailers.apple.com/trailers/home/feeds/%s.json'
+AMT_JSON_URL = 'http://%s/trailers/home/feeds/%s.json'
 ALL_VIDEOS_INC = '%s/includes/automatic.html'
 
 ####################################################################################################
@@ -12,7 +11,8 @@ def Start():
 	ObjectContainer.title1 = 'Apple Movie Trailers'
 
 	HTTP.CacheTime = CACHE_1HOUR
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.29.13 (KHTML, like Gecko) Version/6.0.4 Safari/536.29.13'
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1'
+	HTTP.Headers['Host'] = 'trailers.apple.com'
 
 ####################################################################################################
 @handler('/video/amt', 'Apple Movie Trailers')
@@ -25,7 +25,9 @@ def MainMenu():
 	oc.add(DirectoryObject(key=Callback(Categories, name='most_pop'), title=L('most_pop')))
 	oc.add(DirectoryObject(key=Callback(Genres), title=L('genres')))
 	oc.add(DirectoryObject(key=Callback(Studios), title=L('movie_studios')))
-	oc.add(SearchDirectoryObject(identifier='com.plexapp.plugins.amt', title='Search Trailers', prompt='Search for Movie Trailers', term=L('Trailers')))
+
+	if Client.Product not in ('PlexConnect'):
+		oc.add(SearchDirectoryObject(identifier='com.plexapp.plugins.amt', title='Search Trailers', prompt='Search for Movie Trailers', term=L('Trailers')))
 
 	return oc
 
@@ -35,13 +37,13 @@ def Categories(name):
 
 	oc = ObjectContainer(view_group='List', title2=L(name))
 
-	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % name):
+	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % (GetHost(), name)):
 		url = trailer['location']
 		title = trailer['title']
 		thumb = trailer['poster']
 
 		if not thumb.startswith('http://'):
-			thumb = '%s%s' % (AMT_SITE_URL, thumb)
+			thumb = 'http://trailers.apple.com/%s' % thumb.lstrip('/')
 
 		thumb_large = thumb.replace('poster.jpg', 'poster-large.jpg')
 		thumb_xlarge = thumb.replace('poster.jpg', 'poster-xlarge.jpg')
@@ -49,7 +51,7 @@ def Categories(name):
 		oc.add(DirectoryObject(
 			key = Callback(Videos, url=url, title=title),
 			title = title,
-			thumb = Resource.ContentsOfURLWithFallback([thumb_xlarge, thumb_large, thumb])
+			thumb = Callback(GetThumb, thumbs=[thumb_xlarge, thumb_large, thumb], client=Client.Product)
 		))
 
 	return oc
@@ -61,7 +63,7 @@ def Genres():
 	oc = ObjectContainer(view_group='List', title2=L('genres'))
 	genres = []
 
-	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % 'genres'):
+	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % (GetHost(), 'genres')):
 		for genre in trailer['genre']:
 			if genre not in genres:
 				genres.append(genre)
@@ -82,14 +84,14 @@ def Genre(genre):
 
 	oc = ObjectContainer(view_group='List', title2=genre)
 
-	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % 'genres'):
+	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % (GetHost(), 'genres')):
 		if genre in trailer['genre']:
 			url = trailer['location']
 			title = trailer['title']
 			thumb = trailer['poster']
 
 			if not thumb.startswith('http://'):
-				thumb = '%s%s' % (AMT_SITE_URL, thumb)
+				thumb = 'http://trailers.apple.com/%s' % thumb.lstrip('/')
 
 			thumb_large = thumb.replace('poster.jpg', 'poster-large.jpg')
 			thumb_xlarge = thumb.replace('poster.jpg', 'poster-xlarge.jpg')
@@ -97,7 +99,7 @@ def Genre(genre):
 			oc.add(DirectoryObject(
 				key = Callback(Videos, url=url, title=title),
 				title = title,
-				thumb = Resource.ContentsOfURLWithFallback([thumb_xlarge, thumb_large, thumb])
+				thumb = Callback(GetThumb, thumbs=[thumb_xlarge, thumb_large, thumb], client=Client.Product)
 			))
 
 	return oc
@@ -109,7 +111,7 @@ def Studios():
 	oc = ObjectContainer(view_group='List', title2=L('genres'))
 	studios = []
 
-	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % 'studios'):
+	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % (GetHost(), 'studios')):
 		if trailer['studio'] not in studios:
 			studios.append(trailer['studio'])
 
@@ -129,14 +131,14 @@ def Studio(studio):
 
 	oc = ObjectContainer(view_group='List', title2=studio)
 
-	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % 'studios'):
+	for trailer in JSON.ObjectFromURL(AMT_JSON_URL % (GetHost(), 'studios')):
 		if studio == trailer['studio']:
 			url = trailer['location']
 			title = trailer['title']
 			thumb = trailer['poster']
 
 			if not thumb.startswith('http://'):
-				thumb = '%s%s' % (AMT_SITE_URL, thumb)
+				thumb = 'http://trailers.apple.com/%s' % thumb.lstrip('/')
 
 			thumb_large = thumb.replace('poster.jpg', 'poster-large.jpg')
 			thumb_xlarge = thumb.replace('poster.jpg', 'poster-xlarge.jpg')
@@ -144,7 +146,7 @@ def Studio(studio):
 			oc.add(DirectoryObject(
 				key = Callback(Videos, url=url, title=title),
 				title = title,
-				thumb = Resource.ContentsOfURLWithFallback([thumb_xlarge, thumb_large, thumb])
+				thumb = Callback(GetThumb, thumbs=[thumb_xlarge, thumb_large, thumb], client=Client.Product)
 			))
 
 	return oc
@@ -156,7 +158,9 @@ def Videos(url, title):
 	oc = ObjectContainer(view_group='InfoList', title2=title)
 
 	if not url.startswith('http://'):
-		url = '%s%s' % (AMT_SITE_URL, url)
+		url = 'http://%s/%s' % (GetHost(), url.lstrip('/'))
+	else:
+		url = 'http://%s/%s' % (GetHost(), url.split('.apple.com/')[-1])
 
 	inc_html = HTML.ElementFromURL(ALL_VIDEOS_INC % url.strip('/'))
 
@@ -164,7 +168,7 @@ def Videos(url, title):
 		video = video.split('/')[1]
 
 		try:
-			oc.add(URLService.MetadataObjectForURL('%s#%s' % (url, video)))
+			oc.add(URLService.MetadataObjectForURL('%s#%s' % (url.replace('www.apple.com', 'trailers.apple.com'), video)))
 		except:
 			pass
 
@@ -172,3 +176,26 @@ def Videos(url, title):
 		return ObjectContainer(header="Empty", message="There aren't any items")
 
 	return oc
+
+####################################################################################################
+def GetHost():
+
+	if Client.Product == 'PlexConnect':
+		return 'www.apple.com'
+	else:
+		return 'trailers.apple.com'
+
+####################################################################################################
+def GetThumb(thumbs=[], client=''):
+
+	for url in thumbs:
+		if client == 'PlexConnect':
+			url = url.replace('http://trailers.apple.com/', 'http://www.apple.com/')
+
+		try:
+			data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
+			return DataObject(data, 'image/jpeg')
+		except:
+			continue
+
+	return Redirect('http://resources-cdn.plexapp.com/image/source/com.plexapp.plugins.amt.jpg')
